c2b99270f36a82a24fed5942c645775884ef9fbd
daseme <kurt.olmstead@gmail.com>
2026-08-24 17:58:55 -0700

    revert to inline key, keep info link

--- functions/api/price.js
+++
@@ -1,8 +0,0 @@
-export async function onRequest(context) {
-  const { env } = context;
-  const res = await fetch(`https://finnhub.io/api/v1/quote?symbol=BNO&token=${env.FINNHUB_KEY}`);
-  const data = await res.json();
-  return new Response(JSON.stringify(data), {
-    headers: { "Content-Type": "application/json" }
-  });
-}
